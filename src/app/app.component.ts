15c17aad65bae998a0bc7904af5db08b2e3b298b
Cherhoune Badr <badr.cherhoune@capgemini.com>
2023-07-27 22:17:34 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,176 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'node_modules/chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-chart';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.test2();
+    this.createChart();
+    this.Chart3();
+    this.createChart4();
+    this.createChart5();
+  }
+
+  test2(){
+    var myChart = new Chart("myChart", {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  }
+
+  createChart4(){
+
+    var chart = new Chart("MyChart4", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['Red', 'Pink','Green','Yellow','Orange','Blue', ],
+	       datasets: [{
+    label: 'My First Dataset',
+    data: [300, 240, 100, 432, 253, 34],
+    backgroundColor: [
+      'red',
+      'pink',
+      'green',
+			'yellow',
+      'orange',
+      'blue',			
+    ],
+    hoverOffset: 4
+  }],
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+  }
+
+
+  createChart(){
+  
+    var chart = new Chart("MyChart2", {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2023-01-10', '2023-03-11', '2023-06-12','2023-09-13',
+								 '2023-12-14', '2024-03-15', '2024-06-16','2024-09-17', ], 
+	       datasets: [
+          {
+            label: "Confirmé",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Non Confirmé",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+
+  Chart3(){
+  
+    var chart = new Chart("MyChart3", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+
+
+  createChart5(){
+
+    var chart = new Chart("MyChart5", {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['Red', 'Pink','Green','Yellow','Orange','Blue', ],
+	       datasets: [{
+    label: 'My First Dataset',
+    data: [300, 240, 100, 432, 253, 34],
+    backgroundColor: [
+      'red',
+      'pink',
+      'green',
+			'yellow',
+      'orange',
+      'blue',			
+    ],
+    hoverOffset: 4
+  }],
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+  }
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { MultiformsComponent } from './multiforms/multiforms.component';
+import { MychartComponent } from './mychart/mychart.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    EmployeeComponent,
+    HomeComponent,
+    MainComponent,
+    MultiformsComponent,
+    MychartComponent
   ],
   imports: [
     BrowserModule,
